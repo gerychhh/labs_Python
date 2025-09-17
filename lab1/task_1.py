8d5f89f10e8a3284e1b5529466601c6ad71ed23a
gerychhh <gerkagr29@gmail.com>
2025-09-17 16:18:47 +0300

    add file

--- lab1/task_1.py
+++ lab1/task_1.py
@@ -1,1 +1,5 @@
-print("111111111")
+firstName = input("Enter your first name: ")
+secondName = input("Enter your second name: ")
+surName = input("Enter your surname: ")
+
+print(f"{firstName.title()} {secondName[0].title()}. {surName[0].title()}.")
