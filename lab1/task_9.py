23b0e4a2935510a67f4e1b85a7bff2fb674c47b3
gerychhh <gerkagr29@gmail.com>
2025-09-29 20:54:10 +0300

    новая реализация в таск 9

--- lab1/task_9.py
+++ lab1/task_9.py
@@ -1,8 +1,29 @@
+ip = input("Enter your ip: ")
 
-str = input("Enter your ip: ")
-list = str.split(".")
+p1 = ip.find(".")
+p2 = ip.find(".", p1 + 1)
+p3 = ip.find(".", p2 + 1)
 
-if all(int(x)>=0 for x in list) and all(int(x)<=255 for x in list):
-    print("Good ip")
-else:
+if p1 == -1 or p2 == -1 or p3 == -1:
     print("Bad ip")
+else:
+    part1 = ip[:p1]
+    part2 = ip[p1+1:p2]
+    part3 = ip[p2+1:p3]
+    part4 = ip[p3+1:]
+
+    try:
+        n1 = int(part1)
+        n2 = int(part2)
+        n3 = int(part3)
+        n4 = int(part4)
+
+        if (0 <= n1 <= 255 and
+            0 <= n2 <= 255 and
+            0 <= n3 <= 255 and
+            0 <= n4 <= 255):
+            print("Good ip")
+        else:
+            print("Bad ip")
+    except:
+        print("Bad ip")
