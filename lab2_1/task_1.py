8e06f0c9bf069e2aabce6ef3979765593d688900
gerychhh <gerkagr29@gmail.com>
2025-09-29 22:26:58 +0300

    без библиотеки counter таск 1

--- lab2_1/task_1.py
+++ lab2_1/task_1.py
@@ -1,7 +1,12 @@
-from collections import Counter
-
 text = input("Enter a string: ")
 words = text.lower().split()
-library_of_words = Counter(words)
+
+library_of_words = {}
+
+for word in words:
+    if word in library_of_words:
+        library_of_words[word] += 1
+    else:
+        library_of_words[word] = 1
 
 print(library_of_words)
