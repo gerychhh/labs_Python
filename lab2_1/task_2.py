c14d511f1109de9b7af54674035219d5656ae5aa
gerychhh <gerkagr29@gmail.com>
2025-09-29 22:28:53 +0300

    без библиотеки counter таск 2

--- lab2_1/task_2.py
+++ lab2_1/task_2.py
@@ -1,59 +1,52 @@
-from collections import Counter
-
 myList = list(map(float, input("Enter a list: ").split()))
 
-myList_count = Counter(myList)
-myList_unique =[]
-myList_not_unique =[]
+myList_count = {}
+for num in myList:
+    myList_count[num] = myList_count.get(num, 0) + 1
+
+myList_unique = []
+myList_not_unique = []
 myList_chet = []
 myList_net_chet = []
 myList_float = []
-myList_ = []
+myList_negative = []
 
 print(myList_count)
 
-for i, c in myList_count.items():
-    if c == 1:
-        myList_unique.append(i)
+for num, count in myList_count.items():
+    if count == 1:
+        myList_unique.append(num)
+    else:
+        myList_not_unique.append(num)
 
 print(f"unique: {myList_unique}")
-
-for i, c in  myList_count.items():
-    if c > 1:
-        myList_not_unique.append(i)
-
 print(f"not_unique: {myList_not_unique}")
 
-for i in myList:
-    if i % 2 == 0:
-        myList_chet.append(i)
-print(f"chet: {myList_chet}")
+for num in myList:
+    if num.is_integer():
+        if int(num) % 2 == 0:
+            myList_chet.append(num)
+        else:
+            myList_net_chet.append(num)
 
-for i in myList:
-    if i % 2 != 0:
-        myList_net_chet.append(i)
+print(f"chet: {myList_chet}")
 print(f"net_chet: {myList_net_chet}")
 
-for i in myList:
-    if i < 0:
-        myList_.append(i)
-print(f"numbers < 0: {myList_}")
+for num in myList:
+    if num < 0:
+        myList_negative.append(num)
+print(f"numbers < 0: {myList_negative}")
 
-for i in myList:
-    if isinstance(i, float):
-        myList_float.append(i)
+for num in myList:
+    if isinstance(num, float):
+        myList_float.append(num)
 print(f"float: {myList_float}")
 
 summ = 0
-
-for i in myList:
-    if i % 5 == 0:
-        summ += i
+for num in myList:
+    if num.is_integer() and int(num) % 5 == 0:
+        summ += num
 print(f"summ %5: {summ}")
 
 print(f"Max in your list {max(myList)}")
 print(f"Min in your list {min(myList)}")
-
-
-
-
