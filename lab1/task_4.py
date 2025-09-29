b4cbd7f3fc954657a8ba71ed7508b5e7dff12fb4
gerychhh <gerkagr29@gmail.com>
2025-09-29 21:09:58 +0300

    таск 4 сделал по вашим ограничениям

--- lab1/task_4.py
+++ lab1/task_4.py
@@ -1,27 +1,38 @@
 cash = int(input("Enter your cash: "))
 cash_temp = cash
 
-banknots = {100:0, 50:0, 20:0, 10:0, 5:0, 2:0, 1:0}
+h100 = cash_temp // 100
+cash_temp -= h100 * 100
 
-banknots[100] = cash_temp // 100
-cash_temp -= banknots[100]*100
+h50 = cash_temp // 50
+cash_temp -= h50 * 50
 
-banknots[50] = cash_temp // 50
-cash_temp -= banknots[50]*50
+h20 = cash_temp // 20
+cash_temp -= h20 * 20
 
-banknots[20] = cash_temp // 20
-cash_temp -= banknots[20]*20
+h10 = cash_temp // 10
+cash_temp -= h10 * 10
 
-banknots[10] = cash_temp // 10
-cash_temp -= banknots[10]*10
+h5 = cash_temp // 5
+cash_temp -= h5 * 5
 
-banknots[5] = cash_temp // 5
-cash_temp -= banknots[5]*5
+h2 = cash_temp // 2
+cash_temp -= h2 * 2
 
-banknots[2] = cash_temp // 2
-cash_temp -= banknots[2]*2
+h1 = cash_temp
 
-banknots[1] = cash_temp // 1
-cash_temp -= banknots[1]
-
-print(f"БАНКНОТА : КОЛИЧЕСТВО // {banknots}")
+print(f"To pay {cash} BYN you need:")
+if h100 > 0:
+    print(f"{h100} x 100 BYN")
+if h50 > 0:
+    print(f"{h50} x 50 BYN")
+if h20 > 0:
+    print(f"{h20} x 20 BYN")
+if h10 > 0:
+    print(f"{h10} x 10 BYN")
+if h5 > 0:
+    print(f"{h5} x 5 BYN")
+if h2 > 0:
+    print(f"{h2} x 2 BYN")
+if h1 > 0:
+    print(f"{h1} x 1 BYN")
